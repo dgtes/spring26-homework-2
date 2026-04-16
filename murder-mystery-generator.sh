feb78a690ec0f98a9eefd532b2fcb1ce919b27c4
IvoPetrevski <ivopetrevski@hotmail.com>
2026-04-16 22:54:51 +0300

    Added a new weapon #6

--- murder-mystery-generator.sh
+++ murder-mystery-generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 killers=("Mr. Dosh" "The butler" "Profesor Plum" "The Gardener" "Lady Sterling" "Chef Remi" "Dosh JR.")
-weapons=("an axe" "a candlestick" "a gun" "a dagger" "poison" "a shovel" "a pipe" "a golden trophy")
+weapons=("an axe" "a candlestick" "a gun" "a dagger" "poison" "a shovel" "a pipe" "a golden trophy" "a silver spoon")
 places=("in the kitchen" "in the bathroom" "on the balcony" "in the garden" "in the library" "in the wine cellar" "in the basement")
 reasons=("they cheated" "they were rude" "of a disputed inheritence" "they knew too much" "of blackmail" "of a mistake")
 
